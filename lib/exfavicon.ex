9017bcde5d42b076c6c912c39a20a757871ac2ec
Tatsuo Ikeda <jp.ne.co.jp@gmail.com>
2015-12-07 03:14:21 +0900

    fix finder module

--- lib/exfavicon.ex
+++ lib/exfavicon.ex
@@ -4,7 +4,7 @@ defmodule Exfavicon do
   Finder and detector.
   """
   defdelegate find(url), to: Exfavicon.Finder, as: :find
-  defdelegate find(html, url), to: Exfavicon.Finder, as: :find
+  defdelegate find(url, html), to: Exfavicon.Finder, as: :find
   defdelegate valid_favicon_url?, to: Exfavicon.Finder, as: :valid_favicon_url?
 
 end
--- lib/exfavicon/finder.ex
+++ lib/exfavicon/finder.ex
@@ -2,76 +2,87 @@ defmodule Exfavicon.Finder do
   use HTTPoison.Base
 
   def find(url) do
-    response = url |> request
-    find_from_html(response.body, response.request_url)
+    {:ok, resp} = get(url)
+    detect(resp.body, url)
   end
 
-  def find_from_html(html, url) do
-    favicon_url = find_from_link(html, url) || default_path(url)
-
-    if valid_favicon_url?(favicon_url) do
-      favicon_url
-    else
-      nil
-    end
+  def find(url, html) do
+    detect(html, url)
   end
 
-  defp find_from_link(html, url) do
+  # def find_from_html(html, url) do
+    # favicon_url = find_from_link(html, url) || default_path(url)
 
-    Floki.parse(html).find('//link')
+    # if valid_favicon_url?(favicon_url) do
+      # favicon_url
+    # else
+      # nil
+    # end
+  # end
 
-    Floki.parse(html).find('//link').each do |link|
-      if link[:rel] =~ /^(shortcut )?icon$/i
-        favicon_url_or_path = link[:href]
+  defp detect(html, url) do
+    {:ok, ptn} = Regex.compile("^(shortcut )?icon$", "i") 
 
-        if favicon_url_or_path =~ /^http/
-          return favicon_url_or_path
-        else
-          return URI.join(url, favicon_url_or_path).to_s
+    favicon_url_or_path = 
+      html 
+      |> Floki.find("link") 
+      |> Enum.filter(&(Regex.match?(ptn, List.first(Floki.attribute(&1, "rel")))))
+      |> Enum.flat_map(&(Floki.attribute(&1, "href")))
+      |> List.first
+
+    case favicon_url_or_path do
+      "" ->
+        nil
+      nil ->
+        nil
+      _ ->
+        case Regex.match?(~r/^https?/, favicon_url_or_path) do
+          true ->
+            favicon_url_or_path
+          false ->
+            %{URI.parse(url) | path: favicon_url_or_path}
+            |> URI.to_string
         end
-      end
-    end
-
-    nil
-  end
-
-  defp default_path(url) do
-    uri = URI(url)
-    uri.path = '/favicon.ico'
-    %w[query fragment].each do |element|
-      uri.send element + '=', nil
-    end
-
-    uri.to_s
-  end
-
-  defp request(url, options \\ []) do
-    method = options[:method] || :get
-    max_redirect = options[:max_redirect] || 10
-
-    uri = URI(url)
-    http = Net::HTTP.new(uri.host, uri.port)
-
-    if uri.scheme == 'https'
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    end
-
-    response = http.start do |http|
-      path =
-        (uri.path.empty? ? '/' : uri.path) +
-        (uri.query       ? '?' + uri.query : '') +
-        (uri.fragment    ? '#' + uri.fragment : '')
-      http.send(method, path)
-    end
-
-    if response.kind_of?(Net::HTTPRedirection) && max_redirect > 0
-      request(response['Location'], :max_redirect => max_redirect - 1)
-    else
-      response.instance_variable_set('@request_url', url)
-      def response.request_url; @request_url; end
-      response
     end
   end
+
+  # defp default_path(url) do
+    # uri = URI(url)
+    # uri.path = '/favicon.ico'
+    # %w[query fragment].each do |element|
+      # uri.send element + '=', nil
+    # end
+
+    # uri.to_s
+  # end
+
+  # defp request(url, options \\ []) do
+    # method = options[:method] || :get
+    # max_redirect = options[:max_redirect] || 10
+
+    # uri = URI(url)
+    # http = Net::HTTP.new(uri.host, uri.port)
+
+    # if uri.scheme == 'https'
+      # http.use_ssl = true
+      # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    # end
+
+    # response = http.start do |http|
+      # path =
+        # (uri.path.empty? ? '/' : uri.path) +
+        # (uri.query       ? '?' + uri.query : '') +
+        # (uri.fragment    ? '#' + uri.fragment : '')
+      # http.send(method, path)
+    # end
+
+    # if response.kind_of?(Net::HTTPRedirection) && max_redirect > 0
+      # request(response['Location'], :max_redirect => max_redirect - 1)
+    # else
+      # response.instance_variable_set('@request_url', url)
+      # def response.request_url; @request_url; end
+      # response
+    # end
+  # end
 
 end
